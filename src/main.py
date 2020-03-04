55c4b5e9151de102d8d12de7946334f0e214f2c7
Mark Bonney <mark_b96@protonmail.com>
2020-03-04 20:04:23 +0200

    Created test and train functions

--- src/main.py
+++ src/main.py
@@ -6,6 +6,25 @@ from NeuralNet import NeuralNet
 import numpy as np
 import time
 
+
+def train_model(num_epochs):
+    print('Training...')
+    start_time = time.time()
+    for epoch in range(num_epochs):
+        nn.forward_pass()
+        nn.back_propagation()
+    end_time = time.time()-start_time
+    print('Training completed in: {} seconds'.format(end_time))
+
+
+def test_model():
+    input_data = 'Datasets/q3TestInputs.csv'
+    target_data = 'Datasets/q3TestTargets.csv'
+    nn.read_input_data(input_data, target_data)
+    print('\nError matrix')
+    print(np.round(nn.forward_pass())-nn.output_layer)
+
+
 if __name__ == '__main__':
     learning_rate = 0.1
     epochs = 10_000
@@ -19,17 +38,8 @@ if __name__ == '__main__':
     nn.add_layer(4)
     nn.initialise_weights()
 
-    print('Training...')
-    start_time = time.time()
-    for epoch in range(epochs):
-        nn.forward_pass()
-        nn.back_propagation()
-    end_time = time.time()-start_time
-    print('Training completed in: {} seconds'.format(end_time))
+    train_model(epochs)
+    test_model()
 
-    input_data = 'Datasets/q3TestInputs.csv'
-    target_data = 'Datasets/q3TestTargets.csv'
-    nn.read_input_data(input_data, target_data)
-    print('\nError matrix')
-    print(np.round(nn.forward_pass())-nn.output_layer)
+
 
